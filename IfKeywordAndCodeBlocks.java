2ea2dfe3804cccecc8c768355794aae41a7946a4
Ethan <lockwood.ethan@yahoo.com>
2025-03-31 12:54:19 -0700

    if code block lesson

--- IfKeywordAndCodeBlocks.java
+++ IfKeywordAndCodeBlocks.java
@@ -1,6 +1,27 @@
 public class IfKeywordAndCodeBlocks {
     
     public static void main(String[] args) {
-           
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+        if (gameOver == true) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        gameOver = true;
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+        if (gameOver == true) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
     }
 }

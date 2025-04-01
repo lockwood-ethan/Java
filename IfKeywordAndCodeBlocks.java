e65b999b618d52e6c3c5e9cb000609f2d06e826c
Ethan <lockwood.ethan@yahoo.com>
2025-03-31 20:23:14 -0400

    returning values from a method

--- IfKeywordAndCodeBlocks.java
+++ IfKeywordAndCodeBlocks.java
@@ -2,19 +2,30 @@ public class IfKeywordAndCodeBlocks {
     
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
-        
-        calculateScore(true, 10000, 8, 200);
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("The highScore is " + highScore);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        System.out.println("The next highScore is " +
+            calculateScore(gameOver, score, levelCompleted, bonus));
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         
         int finalScore = score;
 
         if (gameOver == true) {
             finalScore += (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
         }
+        return finalScore;
     }
 }

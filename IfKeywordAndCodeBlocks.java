28f4e2147426ed12b30becdf1f22bf680302321a
Ethan <lockwood.ethan@yahoo.com>
2025-03-31 20:02:33 -0400

    creating and calling methods

--- IfKeywordAndCodeBlocks.java
+++ IfKeywordAndCodeBlocks.java
@@ -2,23 +2,25 @@ public class IfKeywordAndCodeBlocks {
     
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        boolean gameOver;
+        int score;
+        int levelCompleted;
+        int bonus;
 
-        int finalScore = score;
-        if (gameOver == true) {
-            finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(true, 800, 5, 100);
 
         gameOver = true;
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
+        
+        calculateScore(gameOver, score, levelCompleted, bonus);
+    }
 
-        finalScore = score;
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        
+        int finalScore = score;
+
         if (gameOver == true) {
             finalScore += (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);

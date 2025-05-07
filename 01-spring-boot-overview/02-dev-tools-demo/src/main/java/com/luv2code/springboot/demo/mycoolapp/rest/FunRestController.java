4b19d870d7ee8ef86ad2e54496d0e242483a33bf
Ethan <lockwood.ethan@yahoo.com>
2025-05-07 15:10:56 -0400

    added devtools

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,21 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+public class FunRestController {
+    // read data from application.properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${coach.team}")
+    private String teamName;
+
+    // expose a "/" that returns "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World! The coach of the team is: " + coachName + " and the name of the team is: " + teamName;
+    }
+}

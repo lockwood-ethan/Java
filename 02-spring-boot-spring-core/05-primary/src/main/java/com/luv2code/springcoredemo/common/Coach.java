fd5cfedf2e47296f7ba4df3e8dc77a33bc0bfcfb
Ethan <lockwood.ethan@yahoo.com>
2025-05-12 17:00:01 -0400

    added a primary bean to TrackCoach

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/luv2code/springcoredemo/common/Coach.java
@@ -0,0 +1,7 @@
+package com.luv2code.springcoredemo.common;
+
+// The purpose of an interface is not quite clear yet, I am told that they make much more sense in large programs
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // use the @Qualifier annotation to specify which Coach class should be used
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String dailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}

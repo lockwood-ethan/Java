68eeec8d0e3dc77cb4d03f7dd0c0d58f268da697
ethanlockwood <lockwood.ethan@yahoo.com>
2025-05-15 21:50:50 -0400

    test

--- 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -6,16 +6,16 @@ import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class FunRestController {
-    // read data from application.properties
-    @Value("${coach.name}")
-    private String coachName;
+  // read data from application.properties
+  @Value("${coach.name}")
+  private String coachName;
 
-    @Value("${coach.team}")
-    private String teamName;
+  @Value("${coach.team}")
+  private String teamName;
 
-    // expose a "/" that returns "Hello World"
-    @GetMapping("/")
-    public String sayHello() {
-        return "Hello World! The coach of the team is: " + coachName + " and the name of the team is: " + teamName;
-    }
+  // expose a "/" that returns "Hello World"
+  @GetMapping("/")
+  public String sayHello() {
+    return "Hello World! The coach of the team is: " + coachName + " and the name of the team is: " + teamName;
+  }
 }
